24919db403ef2cf988fcb800ba3ea35fc6f03758
31shraddha <31shraddhajain@gmail.com>
2021-08-03 15:01:22 +0530

    API-Gateway and some changes

--- first_part/createResourcesTerraform/main.tf
+++ first_part/createResourcesTerraform/main.tf
@@ -3,32 +3,77 @@ provider "aws" {
 }
 
 
-
-resource "aws_licensemanager_license_configuration" "example" {
-  name                     = "Example"
-  description              = "Example"
-  // license_count            = 10
-  // license_count_hard_limit = true
-  license_counting_type    = "Socket"
-
-  // license_rules = [
-  //   "#minimumSockets=2",
-  // ]
-
-  tags = var.tags
-}
-
-
-
-
-
-
-
 #success
 // module "elasticache" {
 //   source  = "./modules/elasticache"
 //   tags = var.tags
 // }
+
+// resource "aws_api_gateway_rest_api" "example" {
+//   body = jsonencode({
+//     openapi = "3.0.1"
+//     info = {
+//       title   = "example"
+//       version = "1.0"
+//     }
+//     paths = {
+//       "/path1" = {
+//         get = {
+//           x-amazon-apigateway-integration = {
+//             httpMethod           = "GET"
+//             payloadFormatVersion = "1.0"
+//             type                 = "HTTP_PROXY"
+//             uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+//           }
+//         }
+//       }
+//     }
+//   })
+//   name = "example"
+//   endpoint_configuration {
+//     types = ["REGIONAL"]
+//   }
+// }
+
+// resource "aws_api_gateway_deployment" "example" {
+//   rest_api_id = aws_api_gateway_rest_api.example.id
+//   triggers = {
+//     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example.body))
+//   }
+//   lifecycle {
+//     create_before_destroy = true
+//   }
+// }
+
+// resource "aws_api_gateway_stage" "example" {
+//   deployment_id = aws_api_gateway_deployment.example.id
+//   rest_api_id   = aws_api_gateway_rest_api.example.id
+//   stage_name    = "example"
+// }
+
+
+// resource "aws_licensemanager_license_configuration" "example" {
+//   name                     = "Example"
+//   description              = "Example"
+//   // license_count            = 10
+//   // license_count_hard_limit = true
+//   license_counting_type    = "Socket"
+//   // license_rules = [
+//   //   "#minimumSockets=2",
+//   // ]
+//   tags = var.tags
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 #success ECS:CreateService 
 // module "ecs" {
